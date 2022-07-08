e966603b125b14f4867067672d5d1e8f883dea11
Mikkidu <mikkidu1@gmail.com>
2022-07-08 16:33:28 +0300

    --quiet

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,25 +5,18 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     //player
-    public GameObject player;
-    
-    private Vector3 startPosition;
-    private Quaternion startRotation;
+    public Transform player;
 
-    
     // Start is called before the first frame update
     void Start()
-    {  
-        startPosition = transform.position;
-        startRotation = transform.rotation;
-        
+    {
+
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.rotation = Quaternion.Slerp(transform.rotation, player.transform.rotation, Time.deltaTime * 10.0f);
-       
-        transform.position = player.transform.position;
+        transform.rotation = Quaternion.Slerp(transform.rotation, player.rotation, Time.deltaTime * 10.0f);
+        transform.position = player.position;
     }
 }
